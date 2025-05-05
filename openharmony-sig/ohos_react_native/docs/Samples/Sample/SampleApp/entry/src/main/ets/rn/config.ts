3b64d5d8395e3bf6c9022c3d826e581b258971de
Rock Hu <rock.c.c.hu@gmail.com>
2025-05-05 09:19:54 +0000

    openharmony-sig-schedule - 29

--- openharmony-sig/ohos_react_native/docs/Samples/Sample/SampleApp/entry/src/main/ets/rn/config.ts
+++
@@ -1,9 +0,0 @@
-/**
- * Copyright (c) 2024 Huawei Technologies Co., Ltd.
- *
- * This source code is licensed under the MIT license found in the
- * LICENSE-MIT file in the root directory of this source tree.
- */
-
-// ArkTS/CAPI 开关配置
-export const ENABLE_CAPI_ARCHITECTURE = true;
