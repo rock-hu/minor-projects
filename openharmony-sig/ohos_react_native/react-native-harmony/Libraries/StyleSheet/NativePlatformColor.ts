bcfa6dfc5da3454b22c238909f714a32b39e075b
Rock Hu <rock.c.c.hu@gmail.com>
2025-12-29 09:24:25 +0000

    openharmony-sig-schedule - 63

--- openharmony-sig/ohos_react_native/react-native-harmony/Libraries/StyleSheet/NativePlatformColor.ts
+++
@@ -1,8 +0,0 @@
-import { TurboModule, TurboModuleRegistry } from "react-native";
-
-interface Spec extends TurboModule {
-  getSystemColor: () => string | null;
-}
-
-export const NativePlatformColor =
-  TurboModuleRegistry.getEnforcing<Spec>("PlatformColor");
