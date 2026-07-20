a67e7f61dd7e2e079bcd5face06a686a5b1faf52
Rock Hu <rock.c.c.hu@gmail.com>
2026-07-20 11:38:27 +0000

    openharmony-sig-schedule - 92

--- openharmony-sig/ohos_react_native/docs/en/faqs.md
+++
@@ -1,7 +0,0 @@
-# FAQ
-
-- [Compilation Development](./faqs/compilation-faqs.md)
-- [Runtime Development](./faqs/runtime-faqs.md)
-- [Locating Development](./faqs/locating-faqs.md)
-- [Specification Development](./faqs/specification-faqs.md)
-- [Non-standard Usage](./faqs/usage-faqs.md)
--- openharmony-sig/ohos_react_native/tester/tests/TextTest/fixtures.ts
+++
@@ -1,9 +0,0 @@
-/**
- * Copyright (c) 2024 Huawei Technologies Co., Ltd.
- *
- * This source code is licensed under the MIT license found in the
- * LICENSE-MIT file in the root directory of this source tree.
- */
-
-export const SAMPLE_PARAGRAPH_TEXT =
-  'Quis exercitation do eu in laboris nulla sit elit officia. Incididunt ipsum aliquip commodo proident ad laborum aliquip fugiat sunt aute ea laboris mollit reprehenderit. Culpa non incididunt cupidatat esse laborum nulla quis mollit voluptate proident commodo. Consectetur ad deserunt do nulla sunt veniam magna laborum reprehenderit et ullamco fugiat fugiat.';
